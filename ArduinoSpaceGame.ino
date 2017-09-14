1778fcbad876d7d4df9c143d07e45a66d45550f9
HanyFame <hanyfame@yahoo.com>
2017-09-14 02:30:49 -0400

    Update ArduinoSpaceGame.ino

--- ArduinoSpaceGame.ino
+++ ArduinoSpaceGame.ino
@@ -17,11 +17,11 @@ ILI9341_due tft = ILI9341_due(TFT_CS, TFT_DC, TFT_RST);
 
 URTouch  myTouch(34, 32, 30, 28, 26);
 
-int x, y;
-int z = 0;
-int target1, target2, target3;
-int location = 0;
-int targetfall = 0; 
+int x, y; //Touch Location Variables
+int z = 0; //Score Variable
+int target1, target2, target3; //Tagets X location
+int location = 0; //Space Ship X location
+int targetfall = 0; //Targets Y location
 char strBuff[10];
 
 /*************************
@@ -32,27 +32,38 @@ void drawGame()
 {
   tft.fillScreen(ILI9341_BLACK);
   tft.setTextColor(ILI9341_BLUE, ILI9341_BLACK);
-  drawSpace();
   tft.printAlignedOffseted("Score", gTextAlignTopCenter, 120, 8);
   //tft.setSPIClockDivider(2); // lower the frequency if the image is distorted
+  tft.drawImage(fighter2, 135, 140 , 55, 33);
   tft.fillCircle(160, 220, 20, ILI9341_RED);
   tft.fillTriangle(0, 220, 40, 200, 40, 240, ILI9341_RED);
   tft.fillTriangle(320, 220, 280, 200, 280, 240, ILI9341_RED);
-  tft.drawImage(fighter2, 135, 140 , 55, 33);
   tft.fillRect(0, 198, 320, 3, ILI9341_RED);
 }
 
+/*********************
+ * Space BG Function *
+ ********************/
 void drawSpace()
 {
   int ss;
-  tft.fillRect(0, 0, 320, 190, ILI9341_BLACK); 
-  for (ss = 0; ss <=50; ss++)
+  tft.fillRect(0, 0, 320, 190, ILI9341_BLACK);
+  tft.printAlignedOffseted("Score", gTextAlignTopCenter, 120, 8);
+  sprintf(strBuff, "%d", z); 
+  tft.printAlignedOffseted(strBuff, gTextAlignTopCenter, 120, 20); 
+  tft.drawImage(fighter2, 135+8*location, 140, 55, 33);
+  
+  for (ss = 0; ss <=20; ss++)
   {        
-    tft.drawPixel(random(0,320), random(0, 190), ILI9341_CYAN);
-    tft.drawPixel(random(0,320), random(0, 190), ILI9341_WHITE);
+    //tft.drawPixel(random(0,320), random(0, 190), ILI9341_CYAN);
+    tft.drawPixel(random(0,320), random(0, 190), ILI9341_GRAY);    
   }
+  delay(75);
 }
 
+/*******************
+ * Targets Function *
+ ***************** */
 void drawTargets(int y)
 {
   if (y == 0)
@@ -77,10 +88,9 @@ void drawTargets(int y)
 }
 
 
-/*************************
-**  Required functions  **
-*************************/
-
+/************************
+**  Required function  **
+************************/
 
 void setup()
 {
@@ -97,8 +107,13 @@ void setup()
 	drawGame();
 	tft.setTextColor(ILI9341_LIME, ILI9341_BLACK);
   Scheduler.startLoop(loop2);
-  //Scheduler.startLoop(loop3);
+  Scheduler.startLoop(loop3);
 }
+
+
+/***************************
+ * Targets Function Thread *
+ **************************/
   void loop2()
 {
     drawTargets(targetfall);
@@ -110,6 +125,19 @@ void setup()
     yield();
 }
 
+/**************************
+ * Space Bacground Thread *
+ *************************/
+void loop3()
+{
+  drawSpace();
+  yield();
+}
+
+
+/*************
+ * Main Loop *
+ ************/
 void loop()
 {
 	while (true)
@@ -123,18 +151,11 @@ void loop()
 			if ((y >= 200) && (x <= 40))  // left
 			{
         z++;
-        drawSpace();
+        //drawSpace();
 
         tft.fillTriangle(0, 220, 40, 200, 40, 240, ILI9341_BLUE);
         delay(5);        
         tft.fillTriangle(0, 220, 40, 200, 40, 240, ILI9341_RED);
-        
-        //tft.fillRect(0, 20, 320, 50, ILI9341_BLACK);
-        //tft.printAlignedOffseted("LEFT", gTextAlignTopCenter, 0, 30);
-
-        tft.printAlignedOffseted("Score", gTextAlignTopCenter, 120, 8);
-        sprintf(strBuff, "%d", z); 
-        tft.printAlignedOffseted(strBuff, gTextAlignTopCenter, 120, 20);
 
         if ((135+8*(location+1)) >= 265)
         {
@@ -159,23 +180,15 @@ void loop()
         }
 			}
 
-			if ((y >= 160) && (x >=130) && (190 >= x))  // Center
+			if ((y >= 160) && (x >=130) && (190 >= x))  // Shoot Laser
 			{
         z++;
 
-        //drawSpace();
         tft.drawImage(fighter2, 135+8*location, 140, 55, 33);
         
         tft.fillCircle(160, 220, 20, ILI9341_BLUE);
         delay(5);
         tft.fillCircle(160, 220, 20, ILI9341_RED);
-        
-        //tft.fillRect(0, 20, 320, 50, ILI9341_BLACK);
-        //tft.printAlignedOffseted("CENTER", gTextAlignTopCenter, 0, 30);
-
-        tft.printAlignedOffseted("Score", gTextAlignTopCenter, 120, 8);
-        sprintf(strBuff, "%d", z); 
-        tft.printAlignedOffseted(strBuff, gTextAlignTopCenter, 120, 20);
 
         /// Shoot Fighter's Laser
         tft.drawLine(135+(8*location)+26, 140, 135+(8*location)+26, 0, ILI9341_CYAN);
@@ -191,19 +204,10 @@ void loop()
 			if ((y >= 160) && (x >= 280))  // right
 			{
         z++;
-
-        drawSpace();
         
         tft.fillTriangle(320, 220, 280, 200, 280, 240, ILI9341_BLUE);
         delay(5);
         tft.fillTriangle(320, 220, 280, 200, 280, 240, ILI9341_RED);
-        
-        //tft.fillRect(0, 20, 320, 50, ILI9341_BLACK);
-        //tft.printAlignedOffseted("RIGHT", gTextAlignTopCenter, 0, 30);
-        
-        tft.printAlignedOffseted("Score", gTextAlignTopCenter, 120, 8);
-        sprintf(strBuff, "%d", z); 
-        tft.printAlignedOffseted(strBuff, gTextAlignTopCenter, 120, 20);
 
         if ((135+8*(location-1)) <= 0)
         {
@@ -228,6 +232,10 @@ void loop()
           delay(30);              
         }
 			}
+      else
+      {
+        yield();
+      }
 		}
     else
     {
@@ -235,4 +243,3 @@ void loop()
     }
 	}
 }
-
